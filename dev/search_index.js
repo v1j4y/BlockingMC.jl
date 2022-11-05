eba3b7da5df2ceb1df3e8ae1c8efb41a4fdf998d
Documenter.jl <documenter@juliadocs.github.io>
2022-11-05 22:24:41 +0000

    build based on 6a28f97

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BlockingMC","category":"page"},{"location":"#BlockingMC","page":"Home","title":"BlockingMC","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BlockingMC]","category":"page"}]
+}
